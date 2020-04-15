a55a8c555f171163a25810db8b5eff91b040b048
taeil77 <bounggun.yu@gmail.com>
2020-04-15 22:43:25 +0900

    used bean xml

--- BoardWeb/src/main/java/polymorphism/SamsungTV.java
+++ BoardWeb/src/main/java/polymorphism/SamsungTV.java
@@ -2,6 +2,10 @@ package polymorphism;
 
 public class SamsungTV implements TV{
 
+	public SamsungTV() {
+		System.out.println("===> SamsungTV °´Ã¼ »ý¼º");
+	}
+	
 	@Override
 	public void powerOn() {
 		// TODO Auto-generated method stub
--- BoardWeb/src/main/java/polymorphism/TVUser.java
+++ BoardWeb/src/main/java/polymorphism/TVUser.java
@@ -1,12 +1,18 @@
 package polymorphism;
 
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class TVUser {
 	public static void main(String[] args) {
-		BeanFactory factory = new BeanFactory();
-		TV tv = (TV)factory.getBean(args[0]);
+		AbstractApplicationContext factory = new GenericXmlApplicationContext("applicationContext.xml");
+		
+		TV tv = (TV)factory.getBean("tv");
 		tv.powerOn();
 		tv.volumeOn();
 		tv.volumeOff();
 		tv.powerOff();
+		
+		factory.close();
 	}
 }
